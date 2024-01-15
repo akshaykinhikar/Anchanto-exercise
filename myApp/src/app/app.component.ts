25753f26903aa31c9e7fd84597c05d3a5ead0c92
akshaykinhikar <kinhikar.akshay32@gmail.com>
2024-01-15 22:53:27 +0530

    arabic internationalization alongwith css added

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -1,18 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from "@angular/common";
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent  {
 
-  constructor(public translateService: TranslateService) {
+  constructor(public translateService: TranslateService,
+    @Inject(DOCUMENT) private document: Document) {
   }
 
-  public changeLanguage(language: string): void {
-    this.translateService.use(language);
+  public changeLanguage(lang: string) {
+    let htmlTag = this.document.getElementsByTagName(
+      "html"
+    )[0] as HTMLHtmlElement;
+    htmlTag.dir = lang === "ar-UAE" ? "rtl" : "ltr";
+    this.translateService.setDefaultLang(lang);
+    this.translateService.use(lang);
+    this.changeCssFile(lang);
   }
 
+  changeCssFile(lang: string) {
+    let headTag = this.document.getElementsByTagName(
+      "head"
+    )[0] as HTMLHeadElement;
+    let existingLink = this.document.getElementById(
+      "langCss"
+    ) as HTMLLinkElement;
+
+    let bundleName = lang === "ar" ? "arabicStyle.scss" : "englishStyle.scss";
+
+    if (existingLink) {
+      existingLink.href = bundleName;
+    } else {
+      let newLink = this.document.createElement("link");
+      newLink.rel = "stylesheet";
+      newLink.type = "text/css";
+      newLink.id = "langCss";
+      newLink.href = bundleName;
+      headTag.appendChild(newLink);
+    }
+  }
 }
